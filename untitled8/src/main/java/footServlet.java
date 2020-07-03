b37b8a79058cf73513bd686e4dbaa6cd1549286d
@wwwxiangmin1122com <1099965786@qq.com>
2020-07-03 15:55:13 +0800

    '73'

--- untitled8/src/main/java/footServlet.java
+++ untitled8/src/main/java/footServlet.java
@@ -1,10 +1,3 @@
-import org.apache.catalina.Context;
-import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
-import org.apache.tomcat.jni.File;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,19 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/")
-public class HelloServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/foot")
+public class footServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         String name = req.getParameter("name");
         if (name == null) {
-            name = "world";
+            name = "重庆";
         }
-        //在这里链接数据库，获得值就可以了
-
-
         PrintWriter pw = resp.getWriter();
-        pw.write("<h1>Hello, " + name + "!</h1>");
+        pw.write("<h4>nihao, " + name + "!</h4>");
         pw.flush();
     }
 }
